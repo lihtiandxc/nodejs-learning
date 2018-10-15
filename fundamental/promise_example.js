a4ca8e3fd3630d98e471da76ff28a207d55e12c1
LT <llt@hpe.com>
2018-10-15 15:02:23 +0800

    Create promise_example.js

---
+++ fundamental/promise_example.js
@@ -0,0 +1,15 @@
+const fetch = () => {
+    return new Promise((resolve, reject) => {
+        let flag = "false";
+        if (flag === "true") {
+            resolve('{ "text" : "Some awesome text"}');
+        } else {
+            reject('{ "text" : "Rejected"}')
+        }
+
+    })
+}
+
+fetch()
+    .then(result => console.log(JSON.parse(result)))
+    .catch(error => console.log(error));
